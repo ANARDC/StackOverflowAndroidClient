0805743a64117d56a2b4d1e643b10a69ee1d49cc
Anar <anardadashov00@gmail.com>
2020-03-11 08:21:21 +0300

    feat(app): проект создан

---
+++ app/src/main/java/com/commodo/stackoverflow/MainActivity.java
@@ -0,0 +1,14 @@
+package com.commodo.stackoverflow;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
+}
