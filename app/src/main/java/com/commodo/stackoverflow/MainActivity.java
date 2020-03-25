99e584531849f0e433be3665463673aba754ad84
Anar <anardadashov00@gmail.com>
2020-03-25 12:28:40 +0300

    feat(main): создана первая версия PostsRecyclerView
    На главном layout создан компонент RecyclerView, в дополнительном layout создан UI ячейки таблицы. Внутри Adapter объявлен класс ячейки. В методе Adapter onCreateViewHolder создается Context, с помощью которого получаем layout ячейки как Java-объект, осуществляем настройку элемента и возвращаем его. В методе адаптера onBindViewHolder настраиваем элемент при переинициализации. В тот же Adapter передается количество элементов из Activity

--- app/src/main/java/com/commodo/stackoverflow/MainActivity.java
+++ app/src/main/java/com/commodo/stackoverflow/MainActivity.java
@@ -3,12 +3,27 @@ package com.commodo.stackoverflow;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+public final class MainActivity extends AppCompatActivity {
+
+    private RecyclerView postsRecyclerView;
+    private PostsAdapter postsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
+
+        this.postsRecyclerView = findViewById(R.id.posts);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        this.postsRecyclerView.setLayoutManager(layoutManager);
+
+        this.postsRecyclerView.setHasFixedSize(true);
+
+        this.postsAdapter = new PostsAdapter(100);
+        this.postsRecyclerView.setAdapter(this.postsAdapter);
     }
 }
---
+++ app/src/main/java/com/commodo/stackoverflow/PostsAdapter.java
@@ -0,0 +1,68 @@
+package com.commodo.stackoverflow;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public final class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostViewHolder>{
+
+  private static int viewHolderCount;
+  private int numberOfItems;
+
+  public PostsAdapter(int numberOfItems) {
+    this.numberOfItems = numberOfItems;
+    PostsAdapter.viewHolderCount = 0;
+  }
+
+  @SuppressLint("SetTextI18n")
+  @NonNull
+  @Override
+  public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    Context context = parent.getContext();
+    int layoutIdForPostItem = R.layout.posts_list_item;
+
+    LayoutInflater inflater = LayoutInflater.from(context);
+
+    View view = inflater.inflate(layoutIdForPostItem, parent, false);
+
+    PostViewHolder viewHolder = new PostViewHolder(view);
+    viewHolder.viewHolderIndex.setText("ViewHolder index: " + PostsAdapter.viewHolderCount);
+
+    PostsAdapter.viewHolderCount++;
+
+    return viewHolder;
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+    holder.bind(position);
+  }
+
+  @Override
+  public int getItemCount() {
+    return this.numberOfItems;
+  }
+
+  final class PostViewHolder extends RecyclerView.ViewHolder {
+
+    TextView postItemNumberView;
+    TextView viewHolderIndex;
+
+    public PostViewHolder(@NonNull View itemView) {
+      super(itemView);
+
+      this.postItemNumberView = itemView.findViewById(R.id.post_item_number);
+      this.viewHolderIndex = itemView.findViewById(R.id.post_view_holder_number);
+    }
+
+    void bind(int postIndex) {
+      this.postItemNumberView.setText(String.valueOf(postIndex));
+    }
+  }
+}
